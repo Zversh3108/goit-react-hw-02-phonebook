305c07a557ae10c00a9df74fde64598fac3d32b5
Zversh3108 <zvershzt3108@gmail.com>
2023-06-01 16:13:00 +0200

    NewSrc

---
+++ src/components/App/App.jsx
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import AddContactForm from '../AddContactForm/AddContactForm';
+import ContactList from '../ContactList/ContactList';
+import Notification from '../Notification/Notification';
+import { SearchContactByName } from '../SearchContact/SearchContact';
+import { Container } from './App.Styled';
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+  addContact = newContact => {
+    const { contacts } = this.state;
+    if (contacts.find(contact => contact.name === newContact.name)) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    }
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  handleChange = (name, value) => {
+    this.setState({ [name]: value });
+  };
+
+  handleFilterChange = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase().trim();
+
+    if (normalizedFilter === '') {
+      return contacts;
+    }
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { filter } = this.state;
+    const filteredContacts = this.filterContacts();
+
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <AddContactForm
+          addContact={this.addContact}
+          onChange={this.handleChange}
+        />
+        <h2>Contacts</h2>
+        <SearchContactByName
+          title="Find contact by name"
+          onFilterChange={this.handleFilterChange}
+        />
+        {filteredContacts.length > 0 ? (
+          <ContactList
+            contacts={filteredContacts}
+            onDeleteContact={this.deleteContact}
+          />
+        ) : (
+          <Notification
+            message="
+          No name found with this name."
+          />
+        )}
+      </Container>
+    );
+  }
+}
---
+++ src/components/SearchContact/SearchContact.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Text, Input } from './SearchContact.Styled';
+export class SearchContactByName extends Component {
+  render() {
+    return (
+      <div>
+        <Text>{this.props.title}</Text>
+        <form>
+          <Input
+            type="text"
+            onChange={this.props.onFilterChange}
+            placeholder="Search by name"
+          />
+        </form>
+      </div>
+    );
+  }
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    onFilterChange: PropTypes.func.isRequired,
+  };
+}
